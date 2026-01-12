201fb9d88b96c3166a44bedef22e6f72e2095fe3
Bruno AMUZU <amuzuelbruno@gmail.com>
2026-01-12 15:05:06 +0100

    finalisation du JS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ function openMenu(x) {
     x.classList.toggle("change");
     const menu = document.querySelector('.for_burger_menu');
     const breadcrumb = document.querySelector('#breadcrumb');
-    menu.style.display = 'flex';
-    breadcrumb.style.display = 'none';
+    
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+        breadcrumb.style.display = 'block';
+    } else {
+        menu.style.display = 'flex';
+        breadcrumb.style.display = 'none';
+    }
 }
